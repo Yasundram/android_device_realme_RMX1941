5a97344f6c3eecab47579f82c9f319b7e53b723f
Kshitij Gupta <kshitijgm@gmail.com>
2021-10-11 12:17:36 +0530

    RMX1941: Build android.hardware.audio.common@5.0-util
    - Fixes audio crash during boot
    
    Signed-off-by: P-Salik <peersalik16@gmail.com>

--- device.mk
+++ device.mk
@@ -33,6 +33,7 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio.common@5.0-util \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
