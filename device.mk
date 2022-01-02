55b58412eb5e91aae6df457df864ec6ce95294e1
Sarthak Roy <sarthakroy2002@gmail.com>
2022-01-02 13:30:25 +0530

    RMX1941: Stop building wpa_supplicant.conf from source
    Change-Id: I8ccf190132256bb755b354f1807d35ad88cf8dd3
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -380,8 +380,7 @@ PRODUCT_COPY_FILES += \
 
 # WiFi
 PRODUCT_PACKAGES += \
-    WifiOverlay \
-    wpa_supplicant.conf
+    WifiOverlay
 
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0.vendor \
